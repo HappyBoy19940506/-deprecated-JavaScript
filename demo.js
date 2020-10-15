e2bc60b128c21de7fc7d2da5768ed7f40e1d42de
HappyBoy19940506 <HappyBoy19940506@users.noreply.github.com>
2020-10-16 00:55:01 +1100

    JS

--- demo.js
+++ demo.js
@@ -1,1 +1,38 @@
-alert('worinimamam');
+alert(100 + 10);
+// 110
+alert(100 + '10');
+// 10010
+alert(100 - 10);
+// 90
+alert(100 - '20');
+// 80
+alert(null + 'sb');
+//nullsb
+alert(1 + 1);
+// 2
+alert('1' + 2);
+//12
+alert('1'+ '3');
+//13
+alert(1 + 'wori');
+//1wori
+alert(1 + NaN);
+//NaN
+alert('100' - 20);
+//80
+alert('100' - '30');
+//70
+alert(100 - 'a')
+//NaN
+alert( 100 - '100aa')
+//NaN
+alert(NaN + 'wori')
+//NaNwori
+alert(1 + null);
+//1
+alert(1 + undefined);
+//NaN
+alert(1 + true);
+//2
+alert(1 + false);
+//1
