e9862dc9b04f53ac4374c9732233b30cdf04ad02
HappyBoy19940506 <HappyBoy19940506@users.noreply.github.com>
2020-11-11 19:47:21 +1100

    JS

--- demo3.js
+++ demo3.js
@@ -70,5 +70,26 @@ function sum(n){
     }
 }
 
-var res = sum(100);
-alert(res);
+/* var res = sum(100);
+alert(res); */
+
+function tri(){
+    var arr1 = [
+        [1,2,3,4,5],
+        [6,7,8,9,10],
+        [11,12,13,14,15],
+        [16,17,18,19,20],
+        [21,22,23,24,25],
+    ]
+    //arr1[i][j]
+    for(var i = 0;i < 5;i++){
+        for(var j = 0;j <= i;j++){
+            document.write(arr1[i][j] + '&nbsp');
+            if(i == j){
+                document.write('</br>');
+            }
+        }
+    }
+}
+
+tri();
